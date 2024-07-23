cbf1c6951135ff6329d6b1b38eacc544a438788c
Tyrone Francis <tyrone.h.francis@gmail.com>
2024-07-23 07:14:56 +0000

    Added qantity boxes to shopping bag page

--- bag/contexts.py
+++ bag/contexts.py
@@ -23,12 +23,12 @@ def bag_contents(request):
             })
         else:
             product = get_object_or_404(Product, pk=item_id)
-            for size, qantity in item_data['items_by_size'].items():
-                total += qantity * product.price
-                product_count += qantity
+            for size, quantity in item_data['items_by_size'].items():
+                total += quantity * product.price
+                product_count += quantity
                 bag_items.append({
                     'item_id': item_id,
-                    'quantity': item_data,
+                    'quantity': quantity,
                     'product': product,
                     'size': size,
                 })
